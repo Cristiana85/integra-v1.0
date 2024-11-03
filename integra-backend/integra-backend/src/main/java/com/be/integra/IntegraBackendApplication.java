42de70a470f77f778a60b8acd656c3ef8db4844a
cristiana.casanova <cristiana.casanova@optit.net>
2024-11-03 21:06:12 +0100

    Initial commit for frontend and backend

---
+++ integra-backend/integra-backend/src/main/java/com/be/integra/IntegraBackendApplication.java
@@ -0,0 +1,13 @@
+package com.be.integra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegraBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegraBackendApplication.class, args);
+	}
+
+}
