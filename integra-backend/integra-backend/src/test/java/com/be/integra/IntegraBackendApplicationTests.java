4b65b424fcf5094fd8344db30195b6f255eec424
cristiana.casanova <cristiana.casanova@optit.net>
2024-11-03 20:27:54 +0100

    Initial commit with Angular frontend and Spring Boot backend

---
+++ integra-backend/integra-backend/src/test/java/com/be/integra/IntegraBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.be.integra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegraBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
