59df7388543b2c2bc64bd1cfa6a76ffa7c0ec608
Massimo Del prete <max.angular1985@gmail.com>
2024-11-30 23:52:22 +0100

    wip

--- integra-frontend/src/app/editor/components/topbar/topbar.component.ts
+++ integra-frontend/src/app/editor/components/topbar/topbar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { VIEW_PANEL_SIZE } from '../../utilities/editor-constants';
 
 @Component({
   selector: 'integra-topbar',
@@ -9,5 +10,23 @@ import { SharedModule } from 'src/app/shared/shared.module';
   styleUrl: './topbar.component.scss'
 })
 export class TopbarComponent {
+
+  public topbar_height: number = VIEW_PANEL_SIZE.TOPBAR_HEIGHT;
+
+  isEditing: boolean = false; // Tracks if the input is open
+  inputValue: string = 'test'; // Holds the input value
+
+  toggleInput(): void {
+    this.isEditing = true; // Show the input field
+  }
+
+  closeInput(): void {
+    this.isEditing = false; // Hide the input field when focus is lost
+  }
+
+  submitInput(): void {
+    this.isEditing = false; // Submit the input and hide the field
+    console.log('Input submitted:', this.inputValue); // Add your logic here
+  }
 
 }
