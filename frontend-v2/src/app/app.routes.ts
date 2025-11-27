3c612437035afea24a2d6ae69817c190f9c443ec
cristiana.casanova <cristiana.casanova@optit.net>
2025-11-27 10:32:35 +0100

    Delete old prj

--- frontend-v2/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { DiagramComponent } from './diagram/diagram.component';
-
-export const routes: Routes = [
-  { path: '', component: DiagramComponent },
-  // se vuoi, in futuro:
-  // { path: 'diagram', component: DiagramComponent },
-];
